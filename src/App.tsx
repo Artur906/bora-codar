6ab308af44e9ac9c1f573096f55dd470ed41ea3d
Artur Dantas <arturdantasrodrigues@gmail.com>
2023-02-14 11:28:19 -0300

    passing to typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Header from "./components/Header"
-import Challenge02 from "./pages/CompletedChallenges/Challenge02"
-import Challenges from "./pages/Challenges"
+
 import Home from "./pages/Home"
+import Header from "./components/Header"
+import Challenges from "./pages/Challenges"
+
+import Challenge02 from "./pages/CompletedChallenges/Challenge02"
 import Challenge05 from "./pages/CompletedChallenges/Challenge05"
 
 function App() {
